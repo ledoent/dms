959388bdd334746ebfde15edac4c4ef462972236
Don Kendall <dkendall@ledoweb.com>
2026-05-30 10:16:09 -0400

    [IMP] dms_field: regenerate addon READMEs + ruff-format (pre-commit)

--- dms_field/tests/test_dms_field.py
+++ dms_field/tests/test_dms_field.py
@@ -185,9 +185,9 @@ class TestDmsField(BaseCommon):
 
     def test_creation_process_01_with_parent(self):
         self.assertFalse(self.partner.dms_directory_ids)
-        self.template.parent_directory_id = (
-            self.template.storage_id.root_directory_ids[:1]
-        )
+        self.template.parent_directory_id = self.template.storage_id.root_directory_ids[
+            :1
+        ]
         template = self.env["dms.field.template"].with_context(
             res_model=self.partner._name, res_id=self.partner.id
         )
@@ -220,9 +220,9 @@ class TestDmsField(BaseCommon):
         self.assertFalse(partner_2.dms_directory_ids)
 
     def test_creation_process_02_with_parent(self):
-        self.template.parent_directory_id = (
-            self.template.storage_id.root_directory_ids[:1]
-        )
+        self.template.parent_directory_id = self.template.storage_id.root_directory_ids[
+            :1
+        ]
         partner_1 = self.env["res.partner"].create({"name": "Test partner 1"})
         partner_1.invalidate_model()
         directory_1 = partner_1.dms_directory_ids[0]
